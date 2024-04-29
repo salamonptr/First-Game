932bd3307759296c4c77fd646f7f2005b3f1eb89
Salamon Peter <salamonptr@yahoo.com>
2024-04-29 18:41:20 +0300

    gameInput
    GameInput

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,28 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+    private PlayerInputActions _playerInputActions;
+    
+    private void Awake()
+    {
+        _playerInputActions = new PlayerInputActions();
+        _playerInputActions.Player.Move.Enable();
+    }
+
     public Vector2 GetMovementVectorNormalized()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
+        Vector2 inputVector = _playerInputActions.Player.Move.ReadValue<Vector2>();
 
         inputVector = inputVector.normalized;
         return inputVector;
