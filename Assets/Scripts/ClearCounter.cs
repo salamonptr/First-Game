a88e54ba07a95194f10c39e8999e8ec09f6fe2eb
Salamon Peter <salamonptr@yahoo.com>
2024-07-17 20:23:59 +0300

    Revert "ff"
    This reverts commit 6edae8c2cde24bf42cde0ebf549f6dd4f295dc59.

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -6,11 +6,21 @@ public class ClearCounter : MonoBehaviour, IKitchenObjectParent
 {
    [SerializeField] private KitchenObjectSO kitchenObjectsSO;
    [SerializeField] private Transform counterTopPoint;
+   [SerializeField] private ClearCounter secondClearCounter;
+   [SerializeField] private bool testing;
 
    private KitchenObject kitchenObject;
 
-   
-   
+   private void Update()
+   {
+      if (testing && Input.GetKeyDown(KeyCode.T))
+      {
+         if (kitchenObject != null)
+         {
+            kitchenObject.SetKitchenObjectParent(secondClearCounter);
+         }
+      }
+   }
 
    
       public void Interact(Player player)
